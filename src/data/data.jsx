f9763f6081c6f541e21eaf479151c7bbc7e27c3d
naaz <naaz166@gmail.com>
2023-02-15 22:05:33 +0000

    portfolio-react

---
+++ src/data/data.jsx
@@ -0,0 +1,55 @@
+import hobby from '../assets/hobby.png'
+import tda from '../assets/tda.png'
+import quiz from '../assets/quiz.png'
+import weatherAPI from '../assets/weatherAPI.png'
+import hogold from '../assets/hogold.png'
+import bookapi from '../assets/bookapi.png'
+
+
+export const data=[
+  
+    {
+        id:1,
+        name:"Website",
+        image:tda,
+        github:"",
+        live:"https://thedeveloperacademy.com/",
+    },
+    {
+        id:2,
+        name:"Simple Quiz",
+        image:quiz,
+        github:"https://github.com/naaz166/project2quiz",
+        live:"https://naaz166.github.io/project2quiz/",
+    },
+    {
+        id:3,
+        name:"Hobby Website",
+        image:hobby,
+        github:"https://github.com/naaz166/hobby-website",
+        live:"https://naaz166.github.io/hobby-website/",
+    },
+    {
+        id:4,
+        name:"Weather API",
+        image:weatherAPI,
+        github:"https://github.com/naaz166/weather-app ",
+        live:"https://weather-for-cities.herokuapp.com/",
+    },
+    {
+        id:5,
+        name:"Hire Our Grads",
+        image:hogold,
+        github:"https://github.com/naaz166/frontend-hog",
+        live:"https://best-hog-group-app.herokuapp.com/intro",
+    },
+    {
+        id:6,
+        name:"Book List API",
+        image:bookapi,
+        github:"https://github.com/naaz166/Book-list ",
+        live:"",
+    },
+
+
+]
