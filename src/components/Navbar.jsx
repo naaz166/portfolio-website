4ae383fc6e9727bdf750ef716dc24d06a7a142dc
naaz <naaz166@gmail.com>
2023-02-16 07:24:58 +0000

    cv removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Resume from '../assets/cv.pdf'
+
 import {
   FaBars,
   FaTimes,
@@ -122,14 +122,14 @@ const Navbar = () => {
               Email <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+          {/* <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
               href={Resume} download = "resume.pdf"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
