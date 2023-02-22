e51e04fdf3040a29f1abe372accc4e3f748fc300
naaz <naaz166@gmail.com>
2023-02-22 12:02:52 +0000

    added signature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import {
 
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
+// import { BsFillPersonLinesFill } from 'react-icons/bs';
 // import Logo from '../assets/logo.png';
  import { Link } from 'react-scroll';
 
@@ -20,6 +20,7 @@ const Navbar = () => {
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
         {/* <img src={Logo} alt='Logo Image' style={{ width: '200px' }} /> */}
+        <h1 className='text-5xl font-signature ml-2'>Naaz</h1>
       </div>
 
       {/* menu */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    fontFamily: {
+      signature:["Great Vibes"]
+    }
   },
+
   plugins: [],
 }
