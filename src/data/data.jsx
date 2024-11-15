8cf1a59ed75a635b1a458cbd32233d3ea1e54d19
naaz <naaz166@gmail.com>
2024-11-15 09:17:15 +0000

    updated pics

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,6 +1,6 @@
 // import hobby from '../assets/hobby.png'
 import tda from '../assets/tda.png'
-import quiz from '../assets/quiz.png'
+import quiz from '../assets/quiz-game.png'
 import weatherAPI from '../assets/weatherAPI.png'
 import hogold from '../assets/hogold.png'
 import hog1 from '../assets/hog1.png'
