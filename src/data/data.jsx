847be92a1eed05bc7882e5a8ecd1a4da498a2dfc
naaz <naaz166@gmail.com>
2024-11-21 10:13:08 +0000

    new project added

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,64 +1,53 @@
 // import hobby from '../assets/hobby.png'
-import tda from '../assets/tda.png'
-import quiz from '../assets/quiz-game.png'
-import weatherAPI from '../assets/weather-forecast.png'
-import hogold from '../assets/hogold.png'
-import hog1 from '../assets/hog1.png'
-import todo from '../assets/todo.png'
-
+import tda from "../assets/tda.png";
+import quiz from "../assets/quiz-game.png";
+import weatherAPI from "../assets/weather-forecast.png";
+import productStore from "../assets/product-store.png";
+import hog1 from "../assets/hog1.png";
+import todo from "../assets/todo.png";
 
 export const data = [
-    {
-        id:1,
-        name:"Hire Our Grads",
-        image:hog1,
-        github:"",
-        live:"https://hiring.thedeveloperacademy.com/"
-    },
-  
-    {
-        id:2,
-        name:"Website",
-        image:tda,
-        github:"",
-        live:"https://thedeveloperacademy.com/",
-    },
-    {
-        id:3,
-        name:"Simple Quiz",
-        image:quiz,
-        github:"https://github.com/naaz166/project2quiz",
-        live:"https://naaz166.github.io/project2quiz/",
-    },
-    // {
-    //     id:3,
-    //     name:"Hobby Website",
-    //     image:hobby,
-    //     github:"https://github.com/naaz166/hobby-website",
-    //     live:"https://naaz166.github.io/hobby-website/",
-    // },
-    {
-        id:4,
-        name:"Weather API",
-        image:weatherAPI,
-        github:"https://github.com/naaz166/weather-app-new",
-        live:"https://weather-app-api-01.netlify.app",
-    },
-    {
-        id:5,
-        name:"Hire Our Grads",
-        image:hogold,
-        github:"https://github.com/naaz166/frontend-hog",
-        live:"https://best-hog-group-app.herokuapp.com/intro",
-    },
-    {
-        id:6,
-        name:"To do App",
-        image:todo,
-        github: "https://github.com/naaz166/todo-app",
-        live:"https://todo-app-list-01.netlify.app",
-        
-    }
+  {
+    id: 1,
+    name: "Hire Our Grads",
+    image: hog1,
+    github: "",
+    live: "https://hiring.thedeveloperacademy.com/",
+  },
 
-
-]
+  {
+    id: 2,
+    name: "Website",
+    image: tda,
+    github: "",
+    live: "https://thedeveloperacademy.com/",
+  },
+  {
+    id: 3,
+    name: "Simple Quiz",
+    image: quiz,
+    github: "https://github.com/naaz166/project2quiz",
+    live: "https://naaz166.github.io/project2quiz/",
+  },
+  {
+    id: 4,
+    name: "Weather API",
+    image: weatherAPI,
+    github: "https://github.com/naaz166/weather-app-new",
+    live: "https://weather-app-api-01.netlify.app",
+  },
+  {
+    id: 5,
+    name: "Product store",
+    image: productStore,
+    github: "https://github.com/naaz166/product-store",
+    live: "https://product-store-aw03.onrender.com",
+  },
+  {
+    id: 6,
+    name: "To do App",
+    image: todo,
+    github: "https://github.com/naaz166/todo-app",
+    live: "https://todo-app-list-01.netlify.app",
+  },
+];
